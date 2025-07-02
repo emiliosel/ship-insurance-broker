7baa00a25b760a5327c5aa2c6122e62e8ab15311
Emilio Selmanaj <emilioselmanaj@192.168.1.31>
2025-07-03 01:31:31 +0300

    Shared fix

--- packages/shared/src/auth/index.ts
+++ packages/shared/src/auth/index.ts
@@ -2,6 +2,5 @@ export * from "./types";
 export * from "./jwt.service";
 export * from "./jwt-auth.guard";
 export * from "./roles.guard";
-export * from "./tenant.guard";
 export * from "./public.decorator";
 export * from "./auth.module";
